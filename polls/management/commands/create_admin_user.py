62ad086c2aaea80f80ef490476e8ff90fcf11d54
Guandjoy <guandjoy@gmail.com>
2019-07-16 19:10:51 +0300

    Prepare example

---
+++ polls/management/commands/create_admin_user.py
@@ -0,0 +1,21 @@
+import sys
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+
+class Command(BaseCommand):
+    help = 'Creates the initial admin user'
+
+    def handle(self, *args, **options):
+        if User.objects.filter(username="admin").exists():
+            print("admin exists")
+        else:
+            u = User(username='admin')
+            u.set_password('adminpass')
+            u.is_superuser = True
+            u.is_staff = True
+            u.save()
+            print("admin created")
+        sys.exit()
